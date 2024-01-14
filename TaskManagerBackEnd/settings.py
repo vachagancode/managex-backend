1fbf24c1fe1778c3baad3554fd83b669f1230a9c
vachagancode <140316187+vachagancode@users.noreply.github.com>
2024-01-14 19:58:41 +0400

    Update settings.py

--- TaskManagerBackEnd/settings.py
+++ TaskManagerBackEnd/settings.py
@@ -128,5 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://manage-x.vercel.app"
     # Add any other origins as needed
 ]
